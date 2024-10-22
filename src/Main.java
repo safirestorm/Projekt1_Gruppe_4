efde58045b53898200b75194d880e806d26ff56d
Joadoa1992 <jolu001@stud.kea.dk>
2024-10-22 15:34:22 +0200

    Yay

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("*********************");
+        System.out.println("*Joakim F. Lundemann*");
+        System.out.println("*********************");
+        // Det her viser mit navn med stjerner
+    }
+}
