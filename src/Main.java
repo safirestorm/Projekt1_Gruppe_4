603e482014529fddc57f3f3d35f55e7bba94a045
safirestorm <sarahfagerstroem@gmail.com>
2024-10-23 11:27:17 +0200

    Har lavet main metode om til Salon

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("*********************");
-        System.out.println("*Joakim F. Lundemann*");
-        System.out.println("*********************");
-        // Det her viser mit navn med stjerner
-    }
-}
