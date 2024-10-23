f1c87a2068c3910bb469fb41662004919feda830
guneyozcan <guoz0001@stud.kea.dk>
2024-10-23 11:25:15 +0200

    Har tilføjet Aftaler-klasse

---
+++ src/Aftaler.java
@@ -0,0 +1,29 @@
+package src;
+
+import java.time.LocalDate;
+
+
+
+public class Aftaler {
+    String navn;
+    LocalDate dato;
+    int bookingtid;
+    double beløb;
+
+    Aftaler(String navn, LocalDate dato, int bookingtid){
+        this.navn = navn;
+       this.dato = dato;
+        this.bookingtid = bookingtid;
+        beløb = 250;
+
+    }
+
+
+    public String toString() {
+        return navn + " "  + dato + " " +"klokken "+ bookingtid + ":00 " + beløb +" kroner";
+    }
+    public static void main(String[] args) {
+        Aftaler a1 = new Aftaler("Bob",LocalDate.of(2024,10,23), 13);
+        System.out.println( a1);
+    }
+}
