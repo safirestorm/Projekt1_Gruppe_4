47304a24f451631108501eba884c33f8829abc5b
Joadoa1992 <jolu001@stud.kea.dk>
2024-10-24 12:42:13 +0200

    Forbedret Kalender med at adde en booleab sin tjekker om der er en booking eller ej

--- src/Kalender.java
+++ src/Kalender.java
@@ -10,18 +10,24 @@ public class Kalender {
         ArrayList<Aftaler> bookinger= new ArrayList<>();
         bookinger.add(new Aftaler("Bob",LocalDate.of(2024,03,22), 13));
         bookinger.add(new Aftaler("Bob",LocalDate.of(2024,03,22), 11));
-        bookinger.add(new Aftaler("Bob",LocalDate.of(2024,03,22), 13));
+        bookinger.add(new Aftaler("Bob",LocalDate.of(2024,03,22), 10));
 
 
         for(int a=10; a<18; a++) {
             System.out.println("Klokken: "+a+":00");
+            boolean harBooking = false;
 
         for(Aftaler b:bookinger){
                 if (b.dato.isEqual(LocalDate.of(2024, 03, 22)) && b.bookingtid == a) {
                     System.out.println(b);
+                    harBooking = true;
                 }
             }
+            if (!harBooking) {
+                System.out.println("Ledig");
+            }
         }
+
 
     }
 }
