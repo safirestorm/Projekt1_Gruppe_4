1560aa9d393bbc8c02f3875b51bddd537decfa9d
safirestorm <sarahfagerstroem@gmail.com>
2024-10-24 14:47:22 +0200

    Startet med at lave en hovedmenu

--- src/Salon.java
+++ src/Salon.java
@@ -1,4 +1,20 @@
 package src;
 
+import java.util.Scanner;
+
 public class Salon {
+    public static void main(String[] args) {
+
+        while (true) {
+            System.out.println("Velkommen til Harry's Salon Bookingsystem.");
+            System.out.println("Hvad ønsker du at foretage dig?");
+            System.out.println("Tast 1: Booking");
+            System.out.println("Tast 2: Økonomi");
+            System.out.println("Indtast et tal:");
+            Scanner keyboard = new Scanner(System.in);
+
+
+        }
+
+    }
 }
